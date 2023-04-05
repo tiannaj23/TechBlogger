433e4706a89f3849dcaab7e1df30fe10d37d1f62
tiannaj23 <tiannaj23@gmail.com>
2023-04-05 15:29:26 -0400

    connected handlebars

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const session = require('express-session');
+const session = require('express-session'); 
+const path = require('path');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -19,12 +22,16 @@ const sess = {
 };
 
 app.use(session(sess));
-
+const hbs = exphbs.create({helpers})
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
