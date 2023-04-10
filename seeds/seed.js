3209ebb2d3e1458dcef87788b56f8a070cdb5073
tiannaj23 <tiannaj23@gmail.com>
2023-04-10 16:38:36 -0400

    edited seeds file

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+const { Post } = require('../models');
+const userData =require('./userData.json');
+const projectData =require('./projectData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const posts of projectData) {
+    await Post.create({
+      ...posts,
+      userId: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
