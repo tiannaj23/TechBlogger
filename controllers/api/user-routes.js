90bde10f6c5aaeb44f6a97069fb8a046e86de0d5
tiannaj23 <tiannaj23@gmail.com>
2023-04-16 18:44:48 -0400

    working to deploy app

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,7 +4,7 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create({
-      name: req.body.name,
+      name: req.body.username,
       email: req.body.email,
       password: req.body.password
     });
