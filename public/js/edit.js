1d46a342b3a475fa2cb73e6f5e1d84accee81a9a
tiannaj23 <tiannaj23@gmail.com>
2023-04-16 21:35:34 -0400

    finished edit.js file

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,1 +1,40 @@
 const postId = document.querySelector('input[name="post-id"]').value;
+
+const editFormHandler = async function(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body = document.querySelector('textarea[name="post-body"]').value;
+
+    await fetch(`/api/post/${postId}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title,
+          body
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      document.location.replace('/dashboard');
+};
+
+const deleteClickHandler = async function() {
+    await fetch(`/api/post/${postId}`, {
+      method: 'DELETE'
+    });
+
+    document.location.replace('/dashboard');
+
+    setTimeout(function (){
+        document.location.reload()  
+       },2000)
+     };
+
+document
+  .querySelector('#edit-post-form')
+  .addEventListener('submit', editFormHandler);
+document
+  .querySelector('#delete-btn')
+  .addEventListener('click', deleteClickHandler);
